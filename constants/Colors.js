1b649a3248670c96913e93f9c399a06603c0c52f
Gabriela.Berdasco <gabriela_berdasco@hotmail.com>
2022-03-07 21:10:56 -0300

    Desafio parcial 11

---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export const Colors = {
+    primary: '#cdc7e5',
+    accent: '#ff755d'
+}
--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -5,19 +5,48 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import ProductsScreen from "../screens/ProductsScreen";
 import ProductDetailScreen from "../screens/ProductDetailScreen";
+import { Platform, StyleSheet } from "react-native";
+import { Colors } from "../constants/Colors";
 
 const Stack = createNativeStackNavigator()
 
 function ShopNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Categories" component={CategoriesScreen} />
+            <Stack.Navigator 
+            initialRouteName="Categories"
+            screenOptions={{
+                headerStyle: styles.header,
+                headerTitleStyle: styles.headerTitle,
+
+            }}
+            >
+                <Stack.Screen 
+                name="Categories" 
+                component={CategoriesScreen} 
+                options={{
+                    headerTitleStyle: {
+                        color: '#fffbdb',
+                    },
+                    title: 'Tienda B',
+                }}
+                />
                 <Stack.Screen name="Products" component={ProductsScreen} />
                 <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    header:{
+        backgroundColor: Platform.OS === 'ios' ? '' : Colors.accent,
+    },
+    headerTitle: {
+        fontWeight: 'bold',
+        fontSize: 28,
+        color: Colors.primary
+    }
+})
 
 export default ShopNavigator
