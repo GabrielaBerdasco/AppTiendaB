907bf9e3a995e1d45ac315a73c1911dbbfa7a1c5
Gabriela.Berdasco <gabriela_berdasco@hotmail.com>
2022-03-30 15:02:47 -0300

    Cambio de API

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 import { API_KEY } from "@env";
 
 function MapPreview({ location, style, children }) {
